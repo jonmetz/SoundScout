29848f7a543bb1318384bf1acf0eb9764d3a0042
Jonathan Metzman <jon.metzman@gmail.com>
2013-10-27 23:42:05 -0400

    Write test to ensure the site index is the searchpage

--- search/tests.py
+++ search/tests.py
@@ -5,3 +5,8 @@ class SearchTest(TestCase):
     def test_search_page_exists(self):
         response = self.client.get('/search/')
         self.assertEqual(response.status_code, 200)
+
+    def test_site_index_is_searchpage(self):
+        index_response = self.client.get('/')
+        search_response = self.client.get('/search/')
+        self.assertEqual(index_response.content, search_response.content)
