5c16ae649728f09f37bc3f21243d200885e25d89
Jonathan Metzman <jon.metzman@gmail.com>
2013-10-29 15:29:56 -0400

    1. Edited skeleton to point to lowercase urls 2. Corrected errors and fixed grammar in, and improved appearance of, coming_soon.html 3. Created views for browse, about, contact, and legal, which all point to does_not_exist, which was also created 4. Added urlconfs for browse, about, contact, and legal

--- SoundScout/urls.py
+++ SoundScout/urls.py
@@ -7,5 +7,14 @@ urlpatterns = patterns('',
                        url(r'^$', 'SoundScout.views.index', name='index'),
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^songs/', include('songs.urls')),
-                       url(r'^search/', include('search.urls')),
+                       url(r'^search/', include('search.urls')),                       
+                       url(r'^browse/', 'SoundScout.views.browse', name='browse'),
+                       url(r'^about/', 'SoundScout.views.about', name='about'),        
+                       url(r'^contact/', 'SoundScout.views.contact', name='contact'),            
+                       url(r'^legal/', 'SoundScout.views.legal', name='legal'),
 )
+
+                    
+
+                           
+
--- SoundScout/views.py
+++ SoundScout/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'search/search.html', [])
+
+def page_does_not_exist(request, title):
+    return render(request, 'coming_soon.html', {"title": title})
+
+def browse(request):
+    return page_does_not_exist(request, "Browse")
+
+def about(request):
+    return page_does_not_exist(request, "About")
+
+def contact(request):
+    return page_does_not_exist(request, "Contact")
+
+def legal(request):
+    return page_does_not_exist(request, "Legal")
